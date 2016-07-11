f55b9fb077cb976aa0195c78c5d13106de577cea
acsauk <acsauk@gmail.com>
2016-07-11 18:02:51 +0100

    complete exercise #3 - update oystercard_spec.rb with comments

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,4 +1,9 @@
+# This is the error: uninitialized constant Oystercard (NameError)
+# This is the file path: /Users/acsauk/Projects/week_2/oystercard/spec/oystercard_spec.rb
+# This is the line number of the error: 2
+# The NameError has appeared because we have not told the spec file where to find the constant
+# Create a constant/class called 'Oystercard' and link it in the spec file using require
 
-desrcibe Oystercard do
-  
+describe Oystercard do
+
 end
