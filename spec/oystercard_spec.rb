4f9605f353efde38f7fa260038a374ad8061a97c
BartJudge <BartJudge@gmail.com>
2016-07-11 19:13:13 +0100

    exercise 4 adding a default balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -3,7 +3,17 @@
 # This is the line number of the error: 2
 # The NameError has appeared because we have not told the spec file where to find the constant
 # Create a constant/class called 'Oystercard' and link it in the spec file using require
+require 'oystercard'
 
 describe Oystercard do
+
+ context 'when a new card is initialized' do
+
+   it 'has a default monetary value of zero' do
+     card = Oystercard.new
+     expect( subject.balance ).to eq(0)
+   end
+
+ end
 
 end
