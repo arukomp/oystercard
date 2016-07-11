cf99498c93b28767edd98e32fae108a8714312ee
Frunez <francesco.nunez@gmail.com>
2016-07-11 18:06:01 +0100

    Step 6 Enforce Max Balance

--- lib/oyster_card.rb
+++ lib/oyster_card.rb
@@ -1,8 +1,13 @@
 class OysterCard
+  LIMIT = 90
+
   attr_reader :balance
 
   def initialize
     @balance = 0
   end
-
+  def top_up(amount)
+    fail "You are exceeding the limit of £#{LIMIT}" if (@balance + amount) > LIMIT
+    @balance += amount
+  end
 end
--- spec/oyster_card_spec.rb
+++ spec/oyster_card_spec.rb
@@ -7,5 +7,12 @@ describe OysterCard do
       expect(subject.balance).to be_a(Fixnum)
     end
   end
-
+  context "Top up" do
+    it 'should top up card by 5' do
+      expect{subject.top_up(5)}.to change{ subject.balance }.by 5
+    end
+    it 'should not allow to top up above limit' do
+      expect{subject.top_up(95)}.to raise_error "You are exceeding the limit of £#{OysterCard::LIMIT}"
+    end
+  end
 end
