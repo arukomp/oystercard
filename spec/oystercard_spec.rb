d7c65d9e8cdbab610260e464de9aa4683e390743
Samed Yalniz <s.yalniz@hotmail.de>
2016-07-12 12:47:13 +0200

    Challenge #05, top_up method

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,9 +1,15 @@
 require "oystercard"
 describe Oystercard do
+  subject(:card){described_class.new}
   context "new card " do
     it "has default balance zero" do
-      card = Oystercard.new
       expect(card.balance).to eq 0
+    end
+  end
+
+  context "changing balance" do
+    it "tops up balance" do
+      expect{card.top_up(5)}.to change{card.balance}.by(5) 
     end
   end
 end
