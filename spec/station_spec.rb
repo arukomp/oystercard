237d0ec4a7ee0a2b6ced33fc06705976d50b5b31
acsauk <acsauk@gmail.com>
2016-07-12 17:21:06 +0100

    completes exercise #13

--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -3,11 +3,10 @@ require 'station'
 describe Station do
 
   describe 'initialize with correct values' do
-    it 'expects station_name and zone to be passed' do
-      expect(subject).to respond_to(:new).with(2).arguments
-    end
-
-    it { is_expected.to respond_to(:new).with(2).arguments}
+    subject{ described_class.new("Kings Cross", 2) }
+    it { expect(subject.station_name).to eq "Kings Cross" }
+    it { expect(subject.zone).to eq 2 }
   end
+
 
 end
