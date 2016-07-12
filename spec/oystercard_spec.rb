b952a088b1f112b60747b632fd9e3e526fdaef1c
Samed Yalniz <s.yalniz@hotmail.de>
2016-07-12 12:38:14 +0200

    Challenge #04, default balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,9 @@
+require "oystercard"
 describe Oystercard do
-  
+  context "new card " do
+    it "has default balance zero" do
+      card = Oystercard.new
+      expect(card.balance).to eq 0
+    end
+  end
 end
