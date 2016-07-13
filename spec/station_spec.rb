eae391366461b13355c042d480bc8ced5f6b80a8
JulieLev <jkgee1@yahoo.co.uk>
2016-07-13 15:01:49 +0100

    Minor edit to change station test name to bank

--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -1,12 +1,12 @@
 require 'station'
 
 describe Station do
-  subject(:station){described_class.new('name', 1)}
+  subject(:station){described_class.new('bank', 1)}
 
   context "on creation" do
 
     it "has a name" do
-      expect(station.name).to eq 'name'
+      expect(station.name).to eq 'bank'
     end
 
     it "is in a zone" do
